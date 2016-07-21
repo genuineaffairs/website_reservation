4a61ee88fcf658bee556d1a2039d5b4bb1fabbf1
hallos <oscar.hall@outlook.com>
2016-07-21 09:22:09 -0400

    Changed checkin and checkout fields to date-fields instead of datetime

--- models/hotel_reservation.py
+++ models/hotel_reservation.py
@@ -5,3 +5,9 @@ class HotelReservation(models.Model):
 
 	# Add field for message from crm.lead-reservation
 	crm_message = fields.Text('Reservation message')
+	# Override checkin and checkout field to change them
+	# to date-fields instead of datetime
+	checkin = fields.Date('Expected-Date-Arrival', required=True,
+							readonly=True, states={'draft': [('readonly',False)]})
+	checkout = fields.Date('Expected-Date-Departure', required=True,
+							readonly=True, states={'draft': [('readonly',False)]})
