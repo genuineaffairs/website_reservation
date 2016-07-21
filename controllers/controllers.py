9908cedd8f566ba7064ea6683bd0f001fb36f810
hallos <oscar.hall@outlook.com>
2016-07-21 12:22:53 -0400

    Room-dropdown list for reservation form

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -5,20 +5,20 @@ from openerp.addons.website_crm.controllers.main import contactus
 class website_reservation(contactus):
 	#Edit contact method to route additional records to contactus-page
 	@http.route(['/page/website.contactus', '/page/contactus'], type='http', auth="public", website=True)
-		def contact(self, **kwargs):
-			values = {}
-			for field in ['description', 'partner_name', 'phone', 'contact_name', 'email_from', 'name']:
-				if kwargs.get(field):
-					values[field] = kwargs.pop(field)
-			values.update(kwargs=kwargs.items())
-			# Add res.country to routed values 
-			Countries = http.request.env['res.country']
-			values.update({'countries': Countries.search([])})
-			# Add hotel.room.type to touted values
-			Roomtypes = http.request.env['hotel.room.type']
-			values.update({'roomtypes': Roomtypes.search([])})
-				
-			return http.request.website.render("website.contactus", values)
+	def contact(self, **kwargs):
+		values = {}
+		for field in ['description', 'partner_name', 'phone', 'contact_name', 'email_from', 'name']:
+			if kwargs.get(field):
+				values[field] = kwargs.pop(field)
+		values.update(kwargs=kwargs.items())
+		# Add res.country to routed values 
+		Countries = http.request.env['res.country']
+		values.update({'countries': Countries.search([])})
+		# Add hotel.room.type to touted values
+		Roomtypes = http.request.env['hotel.room.type']
+		values.update({'roomtypes': Roomtypes.search([])})
+			
+		return http.request.website.render("website.contactus", values)
 
 	#Edit create_lead method to add extra functionality
 	def create_lead(self, request, values, kwargs):
