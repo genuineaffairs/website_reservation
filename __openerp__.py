9cbaa1bdcc66e47e6235830995e1906fed1d1b76
hallos <oscar.hall@outlook.com>
2016-07-20 20:37:03 -0400

    Changed description

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 {
 	'name': 'Website Reservation Form',
-	'description': 'Extends website_crm Contact Form and transforms it to a hotel reservation form',
+	'description': 'Extends website_crm Contact Form and transforms it to a hotel reservation form. Every new reservation-request is created as a lead in crm-module.',
 	'author' : 'Oscar Hall',
 	'depends' : ['website_crm','hotel_reservation'],
 	'data' : ['views/website_reservation.xml', 'views/crm_lead_view.xml', 'views/hotel_reservation_view.xml' ],
