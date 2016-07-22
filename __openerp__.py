885b940dc1894ff457301f9fd48e2bef2b82dbca
hallos <oscar.hall@outlook.com>
2016-07-22 11:02:25 -0400

    Add user/public access rigths to hotel.room.type

--- __openerp__.py
+++ __openerp__.py
@@ -3,5 +3,8 @@
 	'description': 'Extends website_crm Contact Form and transforms it to a hotel reservation form. Every new reservation-request is created as a lead in crm-module.',
 	'author' : 'Oscar Hall',
 	'depends' : ['website_crm','hotel_reservation'],
-	'data' : ['views/website_reservation.xml', 'views/crm_lead_view.xml', 'views/hotel_reservation_view.xml' ],
+	'data' : ['views/website_reservation.xml', 
+				'views/crm_lead_view.xml', 
+				'views/hotel_reservation_view.xml',
+				'security/ir.model.access.csv'],
 }
