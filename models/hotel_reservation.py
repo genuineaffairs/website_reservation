99fc77c38b6ef9afe27ffce1ad4f1f73b1887f52
hallos <oscar.hall@outlook.com>
2016-07-27 15:19:09 -0400

    Hiding time in reservation form

--- models/hotel_reservation.py
+++ models/hotel_reservation.py
@@ -6,9 +6,3 @@ class HotelReservation(models.Model):
 	# Add field for context from crm.lead-reservation
 	crm_message = fields.Text('Message')
 	requested_room = fields.Char('Requested room')
-	# Override checkin and checkout field to change them
-	# to date-fields instead of datetime
-#	checkin = fields.Date('Expected-Date-Arrival', required=True,
-#							readonly=True, states={'draft': [('readonly',False)]})
-#	checkout = fields.Date('Expected-Date-Departure', required=True,
-#							readonly=True, states={'draft': [('readonly',False)]})
