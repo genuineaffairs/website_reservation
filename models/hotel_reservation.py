c3abd2ed961dfad9cd4dea00246ce729e7df30c6
hallos <oscar.hall@outlook.com>
2016-08-11 11:07:32 -0400

    Functionality for dealing with check_in/out times wih timezone

--- models/hotel_reservation.py
+++ models/hotel_reservation.py
@@ -15,6 +15,24 @@ class HotelReservation(models.Model):
 							states={'draft':[('readonly',False)]}, 
 							default=(lambda *a: time.strftime(DEFAULT_SERVER_DATETIME_FORMAT)))
 	
+	@api.model
+	def create(self, vals):	
+		if not vals:
+			vals = {}
+		if self._context is None:
+			self._context = {}
+		vals['reservation_no'] = self.env['ir.sequence'].get('hotel.reservation')
+		# Set checkin/out times greater than 00:00:00 UTC to display the correct dates with timezone
+		# Checkin time needs to be greater than checkout time so one night is less then 24hours to create folio correctly
+		temp_checkin = fields.Datetime.from_string(vals['checkin'])
+		temp_checkout = fields.Datetime.from_string(vals['checkout'])
+		temp_checkin = temp_checkin.replace(temp_checkin.year,temp_checkin.month,temp_checkin.day,17,00,00)
+		temp_checkout = temp_checkout.replace(temp_checkout.year,temp_checkout.month,temp_checkout.day,15,00,00)
+		vals['checkin'] = temp_checkin
+		vals['checkout'] = temp_checkout
+
+		return super(HotelReservation, self).create(vals)
+		
 	# Change state of lead if reservation was generated from a lead
 	# Function is called from hotel.reservation workflow activity
 	@api.multi
